3a16db31e48aae899e9bd23788ed40383934a1a3
Jeffrey <netteyjeff@gmail.com>
2023-08-10 11:45:08 +0000

    Added functionality to display "X" or "O"

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,58 @@
-function Square() {
-  return <button className="square"></button>;
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+// Individual square component
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
 function App() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  // function to display "X" or "O" when a square is clicked
+  function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
+    // making a copy of the squares array
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
 }
 
+// prop types validation
+Square.propTypes = {
+  value: PropTypes.string,
+  onSquareClick: PropTypes.func,
+};
 export default App;
